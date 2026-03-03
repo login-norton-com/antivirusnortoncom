41a94553def4dec2f0ca73bbff6b4f86a08bacae
login-norton-com <fordken98@gmail.com>
2026-03-03 20:18:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<head>
+    <title>Antivirusnortonhub</title>
+</head>
+<style>
+body{
+    background-image: url(./assets/demo.png);
+    background-position: top;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+<body>
+    
+</body>
